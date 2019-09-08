7c6a45648471ebac11e6a64359c020e61b5301ef
devJRL <dev2sponge@gmail.com>
2019-09-08 19:26:11 +0900

    TDD-Refactoring : App ~/~ Route

--- app.js
+++ app.js
@@ -4,16 +4,10 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 
-const indexRouter = require("./routes/index");
+const rootRouter = require("./routes");
+const userRouter = require("./api/user");
 
 const app = express();
-
-// 첫 API 만들기 : 아래 정보는 추후 DB에서 가져오는 정보로 대체될 예정입니다.
-let users = [
-  { id: 1, name: "alice" },
-  { id: 2, name: "burky" },
-  { id: 3, name: "chris" }
-];
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -24,63 +18,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/", indexRouter);
-
-// # TDD-1 GET
-app.get("/users", (req, res) => {
-  req.query.limit = req.query.limit || 10; // undefined이면 10으로 저장
-  const limit = parseInt(req.query.limit, 10); // 10진수로 변환
-  // 숫자 형변환에 실패하여 NaN이 들어간 경우 400 리턴
-  if (Number.isNaN(limit)) return res.status(400).end();
-
-  res.json(users.slice(0, limit));
-});
-
-app.get("/users/:id", (req, res) => {
-  const p_id = parseInt(req.params.id, 10); // 문자열 id를 숫자열로 변환
-  if (Number.isNaN(p_id)) return res.status(400).end(); // 숫자열 변환 실패시 NaN이 되므로 400 반환
-  const f_user = users.filter(user => user.id === p_id)[0]; // 숫자값에 해당하는 배열을 추출
-  if (!f_user) return res.status(404).end(); // 유저를 찾을 수 없어서 존재하지 않는 경우 404 반환
-  res.json(f_user); // JSON 형식으로 반환
-});
-
-// # TDD-2 DELETE
-app.delete("/users/:id", (req, res) => {
-  const p_id = parseInt(req.params.id, 10); // 문자열 id를 숫자열로 반환
-  if (Number.isNaN(p_id)) return res.status(400).end(); // 숫자열 변호나 실패시 NaN이 되므로 400 반환
-  users = users.filter(user => user.id !== p_id);
-  res.status(204).end();
-});
-
-// # TDD-3 POST
-app.post("/users", (req, res) => {
-  const name = req.body.name;
-  if (!name) return res.status(400).end(); // Name값이 비어있는 경우 400 리턴
-  const conflicLength = users.filter(user => user.name === name).length;
-  if (conflicLength) return res.status(409).end(); // 중복된 경우 409 리턴
-  const id = Date.now();
-  const user = { id, name };
-  users.push(user);
-  res.status(201).json(user);
-});
-
-// # TDD-4 PUT
-app.put("/users/:id", (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  if (Number.isNaN(id)) return res.status(400).end(); // 숫자열 변환에 실패한 경우 400 리턴
-
-  const name = req.body.name;
-  if (!name) return res.status(400).end(); // 전달받은 문자열이 없을 경우 400 리턴
-
-  const isConflict = users.filter(user => user.name === name).length;
-  if (isConflict) return res.status(409).end(); // 중복되는 경우 409 리턴
-
-  const user = users.filter(user => user.id === id)[0]; // 검색
-  if (!user) return res.status(404).end(); // 검색결과 없는 유저일 경우  404 리턴
-
-  user.name = name; // 수정
-
-  res.json(user);
-});
+app.use("/", rootRouter);
+app.use("/users", userRouter);
 
 module.exports = app;
